543a4bb65c4b0c6a1efec8419fa9e377c5838f38
rapismp <112591590+rapismp@users.noreply.github.com>
2022-09-01 13:59:53 -0500

    Update whitelisting.php

--- whitelisting.php
+++ whitelisting.php
@@ -1,22 +1,58 @@
 <?php
 
-if($_POST["message"]) {
+$errors = ”;
 
-mail("rfesko5@gmail.com", "Here is the subject line",
+$myemail = ‘yourname@website.com’;//<—–Put Your email address here. if(empty($_POST[‘name’]) ||
 
-$_POST["insert your message here"]. "From: an@email.address");
+empty($_POST[’email’]) ||
+
+empty($_POST[‘message’]))
+
+{
+
+$errors .= “\n Error: all fields are required”;
+
+}
+
+$name = $_POST[‘name’];
+
+$email_address = $_POST[’email’];
+
+$message = $_POST[‘message’];
+
+if (!preg_match(
+
+“/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i”, $email_address))
+
+{
+
+$errors .= “\n Error: Invalid email address”;
+
+}
+
+if( empty($errors))
+
+{
+
+$to = $myemail;
+
+$email_subject = “Contact form submission: $name”;
+  $email_body = “You have received a new message. “.
+
+” Here are the details:\n Name: $name \n “.
+
+“Email: $email_address\n Message \n $message”;
+
+$headers = “From: $myemail\n”;
+
+$headers .= “Reply-To: $email_address”;
+
+mail($to,$email_subject,$email_body,$headers);
+
+//redirect to the ‘thank you’ page
+
+header(‘Location: contact-form-thank-you.html’);
 
 }
 
 ?>
-
-<!DOCTYPE html>
-<html>
-<form method="post" action="subscriberform.php">
-
-<textarea name="message"></textarea>
-
-<input type="submit">
-
-</form>
-</html>
